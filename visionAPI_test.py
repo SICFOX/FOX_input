5f876dabd8321125192f0c1a62b959e37b50e1d8
Makoto Amano <mako.bouzu@gmail.com>
2018-09-19 22:29:19 +0900

    シリアル通信・例外処理・感情評価

--- visionAPI_test.py
+++ visionAPI_test.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#python label_detection.py (APIキー) (画像ファイルのパス)
+#python visionAPI.py (画像ファイルのパス)
 #api_key AIzaSyCYzlittDbDrELqOMGn77-LYLiuplMnvgA
 
 from base64 import b64encode
 from sys import argv
 import json
 import requests
+import serial
 
 ENDPOINT_URL = 'https://vision.googleapis.com/v1/images:annotate'
+
+#Serial Port
+#ser = serial.Serial('/dev/cu.wchusbserial1420', 9600)
 
 if __name__ == '__main__':
     image_filenames = argv[1:]
@@ -31,13 +35,34 @@ if __name__ == '__main__':
                              params={'key': 'AIzaSyCYzlittDbDrELqOMGn77-LYLiuplMnvgA'},
                              headers={'Content-Type': 'application/json'})
 
-    # for idx, resp in enumerate(response.json()['responses']):
-    #     print(json.dumps(resp, indent=2))
-    a = response.json()['responses'][0]['faceAnnotations'][0]['joyLikelihood']
-    b = response.json()['responses'][0]['faceAnnotations'][0]['sorrowLikelihood']
-    c = response.json()['responses'][0]['faceAnnotations'][0]['angerLikelihood']
-    d = response.json()['responses'][0]['faceAnnotations'][0]['surpriseLikelihood']
-    print("joyLikelihood: "+ a)
-    print("sorrowLikelihood: "+ b)
-    print("angerLikelihood: "+ c)
-    print("surpriseLikelihood: "+ d)
+#IPAdress Error
+    if not image_filenames:
+        print("Please specify API key and image file properly. $ python visionAPI.py image.jpg")
+    else:
+        if response.status_code != 200 or response.json().get('error'):
+            print(response.text)
+
+    if not response.json()['responses'][0]:
+        #ser.write(0)
+        print(0)
+
+    if response.json()['responses'][0]:
+        a = response.json()['responses'][0]['faceAnnotations'][0]['joyLikelihood']
+        b = response.json()['responses'][0]['faceAnnotations'][0]['sorrowLikelihood']
+        c = response.json()['responses'][0]['faceAnnotations'][0]['angerLikelihood']
+        d = response.json()['responses'][0]['faceAnnotations'][0]['surpriseLikelihood']
+
+        emotion = {1:a, 2:b, 3:c, 0:d}
+
+        for key, value in emotion.items():
+            if value == 'VERY_LIKELY':
+                #ser.write(key)
+                print(key)
+
+        if a == 'VERY_UNLIKELY' or b == 'VERY_UNLIKELY' or c == 'VERY_UNLIKELY' or d == 'VERY_UNLIKELY':
+            #ser.write(key)
+            print(0)
+
+
+
+
